c1a0961c362585b1caedd12fac62a0e2064a0535
jerrysong95 <48661921+jerrysong95@users.noreply.github.com>
2022-03-08 21:41:02 -0800

    Add files via upload

--- IGame.sol
+++ IGame.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.0;
 
-/**
+/*
  * @Game Contract
  * @core contract and code
  */
@@ -11,11 +11,11 @@ interface IGame {
 		uint256 attack;
 		uint256 max_health;
 		uint256 current_health;
-		Equipement equipename;
+		Equipment equipename;
     }
 	
 	// equipment attributes
-	struct Equipement {
+	struct Equipment {
 		uint256 sword_strength;
 
     }
@@ -37,13 +37,13 @@ interface IGame {
 	// trade attributes
 	struct Trade {
 		uint256 opponent_money;
-		Equipement equipename;
+		Equipment equipname;
 	}
 
-    mapping(address => Player) players;
-	mapping(adresss => Duel) duel_match;
+    //mapping(address => Player) players;
+	//mapping(address => Duel) duel_match;
 
-    /**
+    /*
      * @notice: Player attacks a monster.
 	 * Depends on the monster's health, attack strength and player's attack strength and health 
 	 * Modifies: monster's health and player's health, Player's equipement and money balance
@@ -52,59 +52,38 @@ interface IGame {
      */
     function attack_monster(uint256 monster_id) external returns(bool result);
 	
-
-	/**
+	/*
      * @notice Player is reborned. Depends on the result of attack_monster
-	 * Modifies: equipement, player health and current health
+	 * Modifies: equipment, player health and current health
      */
-	function reborn() internal;
+	function reborn() external;
 	
-	/**
+	/*
      * @notice invites another player to duel. Depends on you can only send one invite at a time.
 	 * Modifies: duel_match
      */
 	function invite_duel(address player_address) external returns(bool result);
 
-	/**
+	/*
      * @notice accepts another player to duel. Depends if you receive an invite
 	 * Modifies: duel_match, current_health, max_health, opponent_life
      */
 	function accept_duel(address player_address) external returns(bool result);
 	
-	/**
+	/*
      * @notice rejects another player to duel. Depends if you receive an invite
      */
 	function reject_duel(address player_address) external returns(bool result);
 	
-	/**
-     * @notice equipes player with equipment. Depends if equiepment is avaliable
-	 * Modifies: attack_strength
+	/*
+     * @notice equipes player with equipment. Depends if equipment is avaliable
+	 * Modifies: player attack_strength, equipment sale status
      */
 	function equip(uint equipment_id) external;
 	
-	/**
-     * @notice unequipes player with equipment.
-	 * Modifies: attack_strength
+	/*
+     * @notice unequipes player with equipment. Depends if the equipment is equiped
+	 * Modifies: player attack_strength, equipment sale status
      */
 	function unequip(uint equipment_id) external;
-	
-	/**
-     * @notice invites another player to trade.
-     */
-	function invite_trade(address player_address, address equipment_address, address gold_address)  external returns(bool result);
-	
-	/**
-     * @notice accepts another player;s trade offer .
-     */
-	function accept_trade(address player_address)  external returns(bool result);
-	
-	/**
-     * @notice stake an unequipped equipment.
-     */
-	function stake(address equipment_address) internal;
-	
-	/**
-     * @notice unstake and claim the reward.
-     */
-	function unstake(address equipment_address) internal;
 }
