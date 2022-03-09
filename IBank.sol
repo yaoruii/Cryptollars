6196f15c0d74f1767805d3fd194b4ece14dd0f07
FrozenfireMinghang <minghang_li98@163.com>
2022-03-08 17:23:02 -0800

    add more description in it

--- IBank.sol
+++ IBank.sol
@@ -10,7 +10,7 @@ interface IBank {
     /**
      * @notice Player wants to make estimation of his equipment.
      * @param equipment_id the id of the equipment.
-     * @return value the estimated value of equipment, default time is one year.
+     * @return value the estimated value of equipment(the amount of silver coins), default time is one year.
      */
     function estimate(uint256 equipment_id) public returns(unit256 value);
 
@@ -18,7 +18,7 @@ interface IBank {
      * @notice Player stakes his equipment.
      * @param equipment_id the id of the equipment.
      * in this function, the owner of this equipment should change
-     * @return value the real value of equipment after one period time based on the real time (block.timestamp).
+     * @return the amount of silver coins, the real value of equipment after one period time based on the real time (block.timestamp).
      */
     function stake(uint256 equipment_id) public payable returns(unit256 value);
 
@@ -34,7 +34,7 @@ interface IBank {
      * @notice Player does withdrawl after one period time.
      * @param equipment_id the id of the equipment.
      * in this function, the owner of this equipment should change back to the player.
-     * @return value the real value of equipment after one period time.
+     * @return the amount of silver coins, the real value of equipment after one period time.
      */
     function withdraw(uint256 equipment_id) public payable returns(unit256 value);
     
@@ -42,24 +42,24 @@ interface IBank {
      * @notice Player wants to withdrawl in an emergency. Will lose 75% of the reward compared with normal process.
      * @param equipment_id the id of the equipment.
      * in this function, the owner of this equipment should change back to the player.
-     * @return value the value of equipment (with only 25% reward) after one period time.
+     * @return the amount of silver coins, the value of equipment (with only 25% reward) after one period time.
      */
     function withdraw_emergency(uint256 equipment_id) public payable returns(unit256 value);
 
 
     /**
      * @notice Only admin can update the index for getting the accurate compound everyday
-     * @param input are the the id of the equipment and the index value.
+     * @param input is the index value, which means the exchange between Gold and Silver Coins.
      * no output
      * require(msg.sender == admin);
      */
-    function updateIndex_Admin(uint256 equipment_id, uint256 index) public ;
+    function updateIndex_Admin(uint256 index) public ;
 
     /**
-     * @notice Everyone can view the index value for thinking whether to stake equipment
-     * @param input is the equipment id 
-     * @return the real-time index of the equipment, using it can calculate the value.
+     * @notice Everyone can view the index value for thinking the exchange between Gold and Silver Coins
+     * @param 
+     * @return the real-time index of the exchange between Gold and Silver Coins, using it can calculate the value.
      */
-    function viewIndex(uint256 equipment_id) public view returns(uint256 index);
+    function viewIndex() public view returns(uint256 index);
 
 }
