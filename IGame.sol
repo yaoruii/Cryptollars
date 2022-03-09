c060065e558453aff484d4b8602f9c62a79b81e0
jerrysong95 <48661921+jerrysong95@users.noreply.github.com>
2022-03-08 20:51:01 -0800

    Add files via upload

--- IGame.sol
+++ IGame.sol
@@ -9,71 +9,84 @@ interface IGame {
 	// player attributes
     struct Player {
 		uint256 attack;
-		uint256 defence;
 		uint256 max_health;
 		uint256 current_health;
-		uint256 money;
-		Equipement;
+		Equipement equipename;
     }
 	
 	// equipment attributes
 	struct Equipement {
-		uint256 sword;
-		uint256 armour;
-		uint256 potion;
-		uint256 knife;
-		uint256 horse;
-		uint256 dragon;
+		uint256 sword_strength;
+
     }
 	
-	// environment attributes
-	struct Environment {
-		uint256 battlefield;
-		uint256 store;
-		uint256 home;
-		uint256 village;
-		uint256 mountains;
-		uint256 forest;
+	// monster attributes
+	struct Monster {
+		uint256 monster_attack;
+		uint256 monster_max_health;
+		uint256 mosnter_current_health;
     }
+	
+	// duel attributes
+	
+	struct Duel {
+		uint256 opponent_life;
+		uint256 opponent_current_life;
+	}
+	
+	// trade attributes
+	struct Trade {
+		uint256 opponent_money;
+		Equipement equipename;
+	}
 
-    mapping(address => player) players;
+    mapping(address => Player) players;
+	mapping(adresss => Duel) duel_match;
 
     /**
-     * @notice Player attacks a monster.
+     * @notice: Player attacks a monster.
+	 * Depends on the monster's health, attack strength and player's attack strength and health 
+	 * Modifies: monster's health and player's health, Player's equipement and money balance
      * @param monster_id the id of a monster.
      * @return result True = kill the monster; False = get killed.
      */
     function attack_monster(uint256 monster_id) external returns(bool result);
 	
+
 	/**
-     * @notice Player is reborned.
+     * @notice Player is reborned. Depends on the result of attack_monster
+	 * Modifies: equipement, player health and current health
      */
 	function reborn() internal;
 	
 	/**
-     * @notice invites another player to duel.
+     * @notice invites another player to duel. Depends on you can only send one invite at a time.
+	 * Modifies: duel_match
      */
 	function invite_duel(address player_address) external returns(bool result);
 
 	/**
-     * @notice accepts another player to duel.
+     * @notice accepts another player to duel. Depends if you receive an invite
+	 * Modifies: duel_match, current_health, max_health, opponent_life
      */
 	function accept_duel(address player_address) external returns(bool result);
 	
 	/**
-     * @notice rejects another player to duel.
+     * @notice rejects another player to duel. Depends if you receive an invite
      */
 	function reject_duel(address player_address) external returns(bool result);
 	
 	/**
-     * @notice equipes player with equipment.
+     * @notice equipes player with equipment. Depends if equiepment is avaliable
+	 * Modifies: attack_strength
      */
-	function equip(address equipment_address) internal;
+	function equip(uint equipment_id) external;
 	
 	/**
      * @notice unequipes player with equipment.
+	 * Modifies: attack_strength
      */
-	function unequip(address equipment_address) internal;
+	function unequip(uint equipment_id) external;
 	
 	/**
      * @notice invites another player to trade.
