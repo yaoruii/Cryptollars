105944d9485e264959eeb234a54097e4b307eb29
jerrysong95 <48661921+jerrysong95@users.noreply.github.com>
2022-03-11 22:11:34 -0800

    Add files via upload

--- IGame.sol
+++ IGame.sol
@@ -13,14 +13,17 @@ interface IGame {
 		uint256 max_health;
 		uint256 current_health;
 		string player_name;
-		array[] equipment_storage;
-		Equipment equipename;
+		Equipment[] equipment_storage;
+		Equipment equipment;
     }
+	
+	Equipment empty = Equipment(0, 12345, "");
 	
 	// equipment attributes
 	struct Equipment {
 		uint256 sword_strength;
-		int equipment_id
+		uint256 equipment_id;
+		string equipment_name;
     }
 	
 	// monster attributes
@@ -46,8 +49,11 @@ interface IGame {
      * @notice Player is reborned. Depends on the result of attack_monster
 	 * Modifies: equipment, player health and current health
      */
-	function reborn() external;
-	
+	function reborn() external{
+		//equipment lost
+		
+		players[message.sender];
+	}
 	/*
      * @notice invites another player to duel. Depends on you can only send one invite at a time.
 	 * Modifies: duel_match
@@ -69,11 +75,31 @@ interface IGame {
      * @notice equipes player with equipment. Depends if equipment is avaliable
 	 * Modifies: player attack_strength, equipment sale status
      */
-	function equip(uint equipment_id) external;
+	function equip(uint equipment_id) external {
+		
+		require(players[message.sender].equipement = empty);
+		
+		for( uint256 = 0; i<equipement_storage.length; i++){
+			if (equipment_storage[i].id == equipment_id{
+			uint256 gear = True;
+			}
+		}
+		require(gear == True);
+		
+		players[message.sender].attack_strength = players[message.sender].attack_strength + equipment_storage[i].sword_strength;
+		
+		players[message.sender].equipment = equipment_storage[i];
+	}
 	
 	/*
      * @notice unequipes player with equipment. Depends if the equipment is equiped
 	 * Modifies: player attack_strength, equipment sale status
      */
 	function unequip(uint equipment_id) external;
+	
+		require(players[message.sender].equipement != empty);
+		
+		players[message.sender].attack_strength = players[message.sender].attack_strength - equipment.sword_strength;
+		
+		players[message.sender].equipment = empty;
 }
