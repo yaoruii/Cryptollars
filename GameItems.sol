ead5832ed860ea110635d77e0ffd9f213cd7d2a0
rui yao <ruiydut@gmail.com>
2022-03-13 18:20:09 -0700

    modify gameitems.sol

--- GameItems.sol
+++ GameItems.sol
@@ -7,16 +7,20 @@ import "./IPlayer.sol";
 import "./Random.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract GameItems is IGameItems, ERC1155, IPlayer{
+contract GameItems is IGameItems, ERC1155, Random{
 
     uint256 public constant GOLD = 0;
     uint256 public constant SILVER = 1;
-    // uint256 public constant sword = 2;//id = 2
-    // uint256 public constant e1 = 3;//id = 3 =>
+    uint256 public constant NEW_SWARD = 2;//
+
 
     uint256 public equipment_counter = 3;
 
-    uint256 public maximum_attack = 12345;
+    uint256 public maximum_attack = 12;
+    uint256 public minimum_attack = 8;
+
+    uint256 public new_sward_attack = 8;
+
 
     Random public random;
 
@@ -27,22 +31,37 @@ contract GameItems is IGameItems, ERC1155, IPlayer{
     mapping(uint256 => Equipment) allEquipments;
 
 
-    constructor() ERC1155(""){
-        // random = _random;
-        mint_money(msg.sender, GOLD, 10**18, "");
-        mint_money(msg.sender, SILVER, 10**27, "");
-        mint_equipment(msg.sender, "");
+    constructor() ERC1155("") Random(minimum_attack){
+        // mint_money(msg.sender, GOLD, 10**18, "");
+        // mint_money(msg.sender, SILVER, 10**27, "");
+        // mint_equipment(msg.sender, "");
+    }
+
+    function get_random_attack() internal returns(uint256){
+        return super.get_random(maximum_attack);
     }
 
 
     /**
-    This function is used to initialzie every player's initial money and equipment.
+    This function is used to initialzie every player's initial equipment: NEW_SWARD.
     player: the address of this player
     */
-    function mint_initialize(address player) public override{
-        mint_money(player, GOLD, 10**18, "");
-        mint_money(player, SILVER, 10**27, "");
-        mint_equipment(player, "");
+    function mint_initialize(address player) internal returns(Equipment memory){
+        // mint_money(player, GOLD, 10**18, "");
+        // mint_money(player, SILVER, 10**27, "");
+        return mint_new_sward(player,  "");
+    }
+
+    /**
+    to: equipment owner's address
+    randomly mint a new equipment and give it to to address
+     */
+    function mint_new_sward(
+        address to,
+        bytes memory data) internal returns(Equipment memory){
+            _mint(to,NEW_SWARD , 1, data);
+            Equipment memory new_equipment = Equipment(new_sward_attack, NEW_SWARD , "new_sward");
+            return new_equipment;
     }
 
     /**
@@ -51,12 +70,13 @@ contract GameItems is IGameItems, ERC1155, IPlayer{
      */
     function mint_equipment(
         address to,
-        bytes memory data) public override{
+        bytes memory data) internal returns(Equipment memory){
             _mint(to,equipment_counter , 1, data);
-            uint256 attack_value = random.get_random(maximum_attack);
+            uint256 attack_value = get_random_attack();
             Equipment memory new_equipment = Equipment(attack_value, equipment_counter , string(abi.encodePacked(prefix_equipment,Strings.toString(equipment_counter))));
             allEquipments[equipment_counter] = new_equipment;
             equipment_counter++;
+            return new_equipment;
     }
 
     /**
@@ -69,7 +89,7 @@ contract GameItems is IGameItems, ERC1155, IPlayer{
         uint256 money_id,
         uint256 amount,
         bytes memory data
-    ) public override{
+    ) internal{
         _mint(to, money_id, amount, data);
     }
 
@@ -80,7 +100,7 @@ contract GameItems is IGameItems, ERC1155, IPlayer{
         address from,
         uint256 id,
         uint256 amount
-    ) public override{
+    ) internal {
         _burn(from, id, amount);
 
     }
@@ -90,22 +110,22 @@ contract GameItems is IGameItems, ERC1155, IPlayer{
     function burn_equipment(
         address from,
         uint256 id
-    ) public override{
+    ) internal{
         _burn(from, id,1);
         delete allEquipments[id];
 
-    }s
+    }
 
 
     /*
     Transfers the equipment of token type `id` from `from` to 'to'
     */
-    function transfer(
+    function transferEquipment(
         address from,
         address to,
         uint256 equipmentId,
         bytes memory data
-    ) public override{
+    ) internal{
         safeTransferFrom(from, to, equipmentId, 1, data);
     }
 
