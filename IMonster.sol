a2f9447b070885145d1f55b2395bf116f7a411d4
rui yao <ruiydut@gmail.com>
2022-03-13 18:24:26 -0700

    all imonster

---
+++ IMonster.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT;
+pragma solidity >=0.8.0;
+
+interface IMonster{
+    struct Monster {
+            uint256 id;
+            uint256 monster_attack;
+            uint256 monster_current_health;
+            string monster_name;
+    }
+
+}
+
