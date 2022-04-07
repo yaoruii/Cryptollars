47faa78a3aee3ceb5551cc006f35fd74c713e18b
FrozenfireMinghang <minghang_li98@163.com>
2022-04-06 18:01:01 -0700

    change trade to equipment

--- frontend/src/components/Guide.js
+++ frontend/src/components/Guide.js
@@ -1,39 +1,35 @@
 import React from "react";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
 import "../index.css";
 import alchemylogo from "../alchemylogo.svg";
 
 const Guide = () => {
-  const hhh = () =>{
+  const hhh = () => {
     alert("Hello! I am an alert box!");
-  }
+  };
 
   //the UI of our component
   return (
     <div id="container">
       <img id="logo" src={alchemylogo}></img>
-      
-      
+
       <div id="container_functions">
-      <button id="function_module" onClick={hhh}>
+        <button id="function_module" onClick={hhh}>
           <span>Hunt Monsters</span>
-      </button>
+        </button>
 
-      <button id="function_module" onClick={hhh}>
-        
+        <button id="function_module" onClick={hhh}>
           <span>Duel</span>
-      </button>
+        </button>
 
-       <button id="function_module" onClick={hhh}>
-          <span>Trade</span>
-      </button>
-      <button id="function_module" onClick={hhh}>
+        <button id="function_module" onClick={hhh}>
+          <span>Equipment</span>
+        </button>
+        <button id="function_module" onClick={hhh}>
           <span>Bank</span>
-      </button>
+        </button>
       </div>
-
-      
     </div>
   );
 };
